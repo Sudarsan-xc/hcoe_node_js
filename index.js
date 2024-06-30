8bcfd046b0f98a31f9a9e8dfedba8addbed034b0
Sudarsan-xc <sudarsansharma2020@gmail.com>
2024-06-30 15:14:04 +0545

    learnt module

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const http = require("http");
+const os = require("os");
 
-http.createServer().listen();
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    //calculate your RAM
+    const myRAM = os.totalmem().toFixed(2);
+    const toogb = myRAM / (1024 * 1024 * 1024);
+    res.end(
+      JSON.stringify({
+        data: toogb + "Gb",
+      })
+    );
+  })
+  .listen(8800);
+//hw
