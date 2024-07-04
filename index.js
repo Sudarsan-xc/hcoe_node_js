1faf445474733b43b981bcdb26cd4406756db991
Sudarsan-xc <sudarsansharma2020@gmail.com>
2024-07-04 22:00:01 +0545

    things done

--- index.js
+++ index.js
@@ -28,21 +28,29 @@
 // );
 
 //events in node
-const events = require("events");
+// const events = require("events");
 
-const eventEmitter = new events.EventEmitter();
+// const eventEmitter = new events.EventEmitter();
 
-const checkout = (list) => {
-  const price = list.price;
-  const updatedPrice = price + 0.13 * price;
-  console.log("Your total price with VAT is " + updatedPrice);
-};
+// const checkout = (list) => {
+//   const price = list.price;
+//   const updatedPrice = price + 0.13 * price;
+//   console.log("Your total price with VAT is " + updatedPrice);
+// };
 
-// creating a new event plus listen
-eventEmitter.on("checkout", (item) => {
-  checkout(item);
-});
+// // creating a new event plus listen
+// eventEmitter.on("checkout", (item) => {
+//   checkout(item);
+// });
 
-// Fire
-const cart = { name: "Shirt", price: 100 };
-eventEmitter.emit("checkout", cart);
+// // Fire
+// const cart = { name: "Shirt", price: 100 };
+// eventEmitter.emit("checkout", cart);
+// node package manager
+// const properCase = require("proper-upper-case");
+// const convert = (text) => {
+//   return properCase(text);
+// };
+// const result = convert("sudarsan SHARMA");
+// console.log(result);
+
--- string.js
+++ string.js
@@ -1,17 +1,17 @@
-const slugify = (word) => {
-  return word.toLowerCase().toString().split(" ").join("-");
-};
+// const slugify = (word) => {
+//   return word.toLowerCase().toString().split(" ").join("-");
+// };
 
-const properCase = (word) => {
-  return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-};
+// const properCase = (word) => {
+//   return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+// };
 
-const truncate = (word) => {
-  const num = prompt("Enter the limit");
-  if (word.length > num) {
-    return word.slice(0, num) + "...";
-  }
-  return word;
-};
+// const truncate = (word) => {
+//   const num = prompt("Enter the limit");
+//   if (word.length > num) {
+//     return word.slice(0, num) + "...";
+//   }
+//   return word;
+// };
 
-module.exports = { slugify, properCase, truncate };
+// module.exports = { slugify, properCase, truncate };
